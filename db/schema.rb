9d4bb6477e3e6560a60747dc9601ae30eb219409
Mr. Marco Prins <snirpocram@gmail.com>
2019-09-14 17:17:42 +0200

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_145822) do
+ActiveRecord::Schema.define(version: 2019_09_14_150712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 2019_09_14_145822) do
     t.integer "movie_id"
     t.integer "stars"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
