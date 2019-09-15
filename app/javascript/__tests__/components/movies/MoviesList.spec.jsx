89d7091c18969a57082eaafa387c84bf81dff1af
Mr. Marco Prins <snirpocram@gmail.com>
2019-09-15 19:08:24 +0200

    Add tests for MoviesList

--- app/javascript/__tests__/components/movies/MoviesList.spec.jsx
+++ app/javascript/__tests__/components/movies/MoviesList.spec.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import wait from 'waait';
+import axios from 'axios';
+
+import MoviesList from '../../../components/movies/MoviesList';
+import Stars from '../../../components/movies/Stars';
+
+const stubAxios = (response) => {
+  axios.get = jest.fn(() => Promise.resolve({
+    data: response,
+  }));
+};
 
 describe('MoviesList', () => {
-  it('is wonderful', () => {
-    expect(true).toBe(true);
+  it('has the same number of headers as columns', async () => {
+    stubAxios([{id: 1, title: 'Movie 1', text: 'Description', averageStars: 2}]);
+
+    const component = mount(<MoviesList />);
+    await wait(0); component.update();
+
+    const headers = component.instance().headers();
+    const firstRow = component.find('table tbody tr').find('td');
+
+    expect(headers.length).toBe(firstRow.length);
+  });
+
+  it('displays the correct rating headers based on wheter user is present', async () => {
+    stubAxios([]);
+
+    const componentWithoutUser = mount(<MoviesList />);
+    expect(componentWithoutUser.instance().headers()[2]).toBe('Average Rating');
+
+    const componentWitUser = mount(<MoviesList user={{id: 1, email: 'test@gmail.com'}} />);
+    expect(componentWitUser.instance().headers()[2]).toBe('Your Rating');
+  });
+
+  it('fetches and displays movies', async () => {
+    stubAxios([
+      {id: 1, title: 'Movie 1', text: 'Description 1', averageStars: 1},
+      {id: 2, title: 'Movie 2', text: 'Description 2', averageStars: 2},
+    ]);
+
+    const component = mount(<MoviesList />);
+    await wait(0); component.update();
+
+    const rows = component.find('table tbody tr');
+    const content = rows.map(row => row.find('td').map(td => td.text()));
+    expect(content).toEqual([
+      [
+        'Movie 1',
+        'Description 1',
+        '',
+      ],
+      [
+        'Movie 2',
+        'Description 2',
+        '',
+      ],
+    ]);
+  });
+
+  xit('fetches new movies with category filters if you click a category', () => {
   });
 });
